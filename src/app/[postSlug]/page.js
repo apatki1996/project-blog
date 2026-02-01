2570f8fc7664ba7d871a74186944a5cfad4b6cbb
apatki1996 <aashwin.patki@gmail.com>
2026-02-01 11:33:57 -0800

    Solution 3

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -5,10 +5,26 @@ import BlogHero from "@/components/BlogHero";
 import styles from "./postSlug.module.css";
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import { BLOG_TITLE } from "@/constants";
+
+const cachedLoadBlogPost = React.cache(async (postSlug) => {
+  return await loadBlogPost(postSlug)
+})
+
+export async function generateMetadata({params}) {
+  const {postSlug} = await params;
+
+  const { frontmatter } = await cachedLoadBlogPost(postSlug);
+
+  return {
+    title: `${frontmatter.title} • ${BLOG_TITLE}`,
+    description: frontmatter.abstract,
+  }
+}
 
 async function BlogPost({ params }) {
   const { postSlug } = await params;
-  const { frontmatter, content } = await loadBlogPost(postSlug);
+  const { frontmatter, content } = await cachedLoadBlogPost(postSlug);
 
   return (
     <article className={styles.wrapper}>
